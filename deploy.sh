88397c4ae9d65b69b629ee17d57b995e677b96c8
Omar Vilera <omarvilera21@gmail.com>
2023-04-07 22:37:48 -0500

    set namespace current context to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@
 
 read -p 'Give a name to the blog: ' BLOGNAME
  
-read -p 'which is the db host: ' DBHOST
- 
 read -p 'which is the db name: ' DBNAME
  
 read -p 'which is the DBUSER: ' DBUSER
@@ -14,13 +12,17 @@ read -p 'which plugins do you want? (separated by commas): ' WPPLUGINS
 
 read -p 'specify a username pls: ' USERNAME
 
+read -p 'Nombre unico para tu apply: ' NAMESPACE
+
+kubectl create ns $NAMESPACE
+
+kubectl config set-context --current --namespace=$NAMESPACE
+
 kubectl create secret generic db-secret --from-literal=rootdb='123456' --from-literal=bitnamidb='123456'
 
 kubectl create secret generic wppass --from-literal=wpsecret='123456' 
 
 kubectl create configmap wpblogname --from-literal=name=$BLOGNAME
-
-kubectl create configmap wpdbhost --from-literal=name=$DBHOST
 
 kubectl create configmap wpdbname --from-literal=name=$DBNAME
 
